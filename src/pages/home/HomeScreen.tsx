f9b0aea31ca8bc7d2a627544735118a084ee14dd
Julien <julien.izzillo@gmail.com>
2023-03-09 14:22:52 +0100

    Adding query at app launch to load data before displaying the app

--- src/pages/home/HomeScreen.tsx
+++ src/pages/home/HomeScreen.tsx
@@ -4,6 +4,9 @@ import {Header} from '../header/Header';
 import {HomeButton} from './components/HomeButton';
 import {HOME_SCREEN_BUTTONS_DATASET} from './components/HomeButtonsData';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useFetchBugs} from '../common/functions/FetchBugs';
+import {useFetchFishes} from '../common/functions/FetchFishes';
+import {LoadingPage} from '../common/components/LoadingPage';
 
 export const NUMBER_OF_COLUMNS = 3;
 export const MAX_GRID_WIDTH = 500;
@@ -14,29 +17,35 @@ const HEADER_IMAGE_PATH = require('../header/img/header_background.jpg');
 
 export const HomeScreen = () => {
   const insets = useSafeAreaInsets();
+  const {data: fishList, isLoading: isFishLoading} = useFetchFishes();
+  const {data: bugList, isLoading: isBugLoading} = useFetchBugs();
 
-  return (
-    <View
-      style={{
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-        flex: 1,
-      }}>
-      <Header
-        headerImagePath={HEADER_IMAGE_PATH}
-        headerText={HEADER_HOME_TEXT}
-      />
-      <View style={styles.listWrapper}>
-        {HOME_SCREEN_BUTTONS_DATASET.map(button => (
-          <HomeButton
-            label={button.label}
-            imagePath={button.imagePath}
-            navigationTarget={button.navigationTarget}
-          />
-        ))}
+  if (!isFishLoading && !isBugLoading) {
+    return (
+      <View
+        style={{
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          flex: 1,
+        }}>
+        <Header
+          headerImagePath={HEADER_IMAGE_PATH}
+          headerText={HEADER_HOME_TEXT}
+        />
+        <View style={styles.listWrapper}>
+          {HOME_SCREEN_BUTTONS_DATASET.map(button => (
+            <HomeButton
+              label={button.label}
+              imagePath={button.imagePath}
+              navigationTarget={button.navigationTarget}
+            />
+          ))}
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else {
+    return <LoadingPage image={HEADER_IMAGE_PATH} text={HEADER_HOME_TEXT} />;
+  }
 };
 
 const styles = StyleSheet.create({
