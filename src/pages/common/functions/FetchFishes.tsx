0e5c6b329437a444ac033391ddcd29c3ebeb1a55
Julien <julien.izzillo@gmail.com>
2023-03-20 18:53:11 +0100

    Switching from .then to async/await for fish fetching

--- src/pages/common/functions/FetchFishes.tsx
+++ src/pages/common/functions/FetchFishes.tsx
@@ -1,10 +1,10 @@
-import {Fishes} from '../../encyclopedie/Types';
+import {Fish} from '../../encyclopedie/Types';
 import {useQuery} from 'react-query';
 
-const FetchFishes = () =>
-  fetch('https://acnhapi.com/v1/fish', {method: 'GET'})
-    .then((response: Response) => response.json())
-    .then((responseJSON: Fishes) => Object.values(responseJSON));
+async function FetchFishes(): Promise<Fish[]> {
+  const response = await fetch('https://acnhapi.com/v1/fish', {method: 'GET'});
+  return Object.values(await response.json());
+}
 
 export const useFetchFishes = () => {
   const {data, isLoading} = useQuery('fishList', FetchFishes);
