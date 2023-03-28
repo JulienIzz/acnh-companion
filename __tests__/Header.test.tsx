2c203052eacb633c682f8a9b66b7bd4cf25cc92f
Julien <julien.izzillo@gmail.com>
2023-03-28 15:12:25 +0200

    Creating header test

---
+++ __tests__/Header.test.tsx
@@ -0,0 +1,23 @@
+import {Header} from '../src/pages/header/Header';
+import React from 'react';
+import {render, screen} from '@testing-library/react-native';
+
+test('header with some text', async () => {
+  // PARTIE 1 : On rend le composant avec les constantes qu'on veut
+  const expectedHeaderTextForTest = 'Mock Text Header';
+  const expectedHeaderImageForTest = require('../src/pages/header/img/header_background.jpg');
+
+  render(
+    <Header
+      headerText={expectedHeaderTextForTest}
+      headerImagePath={expectedHeaderImageForTest}
+    />,
+  );
+
+  // PARTIE 2 : On cherche les données qu'on veut vérifier dans le composant rendu / On vérifie les comportements
+  const HeaderOutput = screen.getByText(expectedHeaderTextForTest);
+
+  // PARTIE 3 : On fait des vérifications
+  expect(HeaderOutput).toBeTruthy();
+  expect(screen.toJSON()).toMatchSnapshot();
+});
