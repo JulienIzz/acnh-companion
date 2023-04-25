b3ac8e2389f5d47294eec5078631025a4342626d
Julien <julien.izzillo@gmail.com>
2023-04-25 17:01:21 +0200

    Excluding Typing files from Jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
     '!src/**/*.test.{ts,tsx,js,jsx}',
+    '!src/**/Types.tsx',
   ],
   coverageReporters: ['text'],
   snapshotResolver: './config/jestSnapshotResolver.js',
