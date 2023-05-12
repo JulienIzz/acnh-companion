60b87083133d384826fd2827ede5e5f99f8db830
Julien <julien.izzillo@gmail.com>
2023-05-12 15:17:53 +0200

    Adding fish & bugs fetch and conditional rendering

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,9 +1,15 @@
 import {Header} from '../header/Header';
 import {View} from 'react-native';
+import {LoadingPage} from '../common/components/LoadingPage';
+import {useFetchBugs} from '../common/functions/FetchBugs';
+import {useFetchFishes} from '../common/functions/FetchFishes';
 const HEADER_IMAGE_PATH = require('../header/img/header_background.jpg');
 const HEADER_SEARCH_TEXT = 'Recherche';
 
 export const SearchPage = () => {
+  const {data: fishList, isLoading: isFishLoading} = useFetchFishes();
+  const {data: bugList, isLoading: isBugLoading} = useFetchBugs();
+  if (!isFishLoading && !isBugLoading) {
     return (
       <View style={{flex: 1}}>
         <Header
@@ -12,4 +18,7 @@ export const SearchPage = () => {
         />
       </View>
     );
+  } else {
+    return <LoadingPage image={HEADER_IMAGE_PATH} text={HEADER_SEARCH_TEXT} />;
+  }
 };
