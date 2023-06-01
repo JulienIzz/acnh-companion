654ac6b321bdc857bf5e29ab0b16551498d59c0d
Julien <julien.izzillo@gmail.com>
2023-06-01 11:42:58 +0200

    cleaning: deleting dead code

--- src/pages/home/HomeScreen.tsx
+++ src/pages/home/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {View, FlatList, ScrollView} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import {Header} from '../header/Header';
 import {HOME_SCREEN_BUTTONS_DATASET} from './components/HomeButtonsData';
