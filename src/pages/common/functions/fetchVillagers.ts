c5e8b665a73ac83fb555df42dd704ca577f7deb9
Julien <julien.izzillo@gmail.com>
2023-06-17 18:23:27 +0200

    Adding my cat in easter egg in the villager list

--- src/pages/common/functions/fetchVillagers.ts
+++ src/pages/common/functions/fetchVillagers.ts
@@ -6,10 +6,27 @@ async function fetchVillagers(): Promise<Villager[]> {
     method: 'GET',
   });
   const data: Villager[] = Object.values(await response.json());
+  data.unshift(Lixy);
   return data;
 }
 
 export const useFetchVillagers = () => {
   const {data, isLoading} = useQuery('villagerList', fetchVillagers);
   return {data, isLoading};
+};
+
+const Lixy: Villager = {
+  id: 0,
+  name: {
+    'name-USen': 'Lixy',
+    'name-EUfr': 'Lixy',
+  },
+  personality: 'Être de colère',
+  'birthday-string': 'August 23th',
+  birthday: '8/23',
+  species: 'Cat',
+  gender: 'Female',
+  'catch-phrase': 'kitty cat',
+  icon_uri: 'https://acnhapi.com/v1/icons/villagers/46',
+  image_uri: 'https://acnhapi.com/v1/images/villagers/46',
 };
