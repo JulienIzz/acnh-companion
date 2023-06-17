17cff51abc2fc2d2d9947942ea9a76b7bd8386b3
Julien <julien.izzillo@gmail.com>
2023-06-17 17:33:32 +0200

    Calling useFetchVillagers query in Villagers page and put a loading page while fetching

--- src/pages/villagers/Villagers.tsx
+++ src/pages/villagers/Villagers.tsx
@@ -1,11 +1,21 @@
 import {View} from 'react-native';
 import React from 'react';
 import {Header} from '../header/Header';
+import {useFetchVillagers} from '../common/functions/fetchVillagers';
+import {LoadingPage} from '../common/components/LoadingPage';
 
 const HEADER_VILLAGERS_TEXT = 'Villageois';
 const HEADER_IMAGE_PATH = require('../header/img/header_background.jpg');
 
 export const Villagers = () => {
+  const {data: villagerList, isLoading: isVillagerLoading} =
+    useFetchVillagers();
+
+  if (isVillagerLoading) {
+    return (
+      <LoadingPage image={HEADER_IMAGE_PATH} text={HEADER_VILLAGERS_TEXT} />
+    );
+  }
   return (
     <View>
       <Header
