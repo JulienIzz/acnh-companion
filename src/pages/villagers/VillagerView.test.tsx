25e604e60a8482722fe51652cdc135041b95759e
Julien <julien.izzillo@gmail.com>
2023-06-21 17:54:09 +0200

    Testing VillagerView

---
+++ src/pages/villagers/VillagerView.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react-native';
+import {VillagerView} from './VillagerView';
+import {mockVillagerArray} from '../../tests/mockVillagerArray';
+
+test('Villager view card with mock information', async () => {
+  render(<VillagerView villager={mockVillagerArray[0]} />);
+
+  const nameOutput = screen.getByText('Nom : Villager1');
+  const personalityOutput = screen.getByText('Personnalité : Personality1');
+  const birthdayOutput = screen.getByText('Anniversaire : January 1st');
+  const sexOutput = screen.getByText('Sexe : Male');
+  const specieOutput = screen.getByText('Espèce : Dragon');
+
+  expect(nameOutput).toBeTruthy();
+  expect(personalityOutput).toBeTruthy();
+  expect(birthdayOutput).toBeTruthy();
+  expect(sexOutput).toBeTruthy();
+  expect(specieOutput).toBeTruthy();
+
+  expect(screen.toJSON()).toMatchSnapshot();
+});
