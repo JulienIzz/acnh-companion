f7aa21eefa5d3d6ba6fe0073bf82721a63fee038
Julien <julien.izzillo@gmail.com>
2023-06-21 17:54:00 +0200

    Creating a mocked Villager

---
+++ src/tests/mockVillagerArray.ts
@@ -0,0 +1,19 @@
+import {Villager} from '../pages/villagers/VillagerType';
+
+export const mockVillagerArray: Villager[] = [
+  {
+    id: 1,
+    name: {
+      'name-USen': 'Villager1',
+      'name-EUfr': 'Villager1',
+    },
+    personality: 'Personality1',
+    'birthday-string': 'January 1st',
+    birthday: '1/1',
+    species: 'Dragon',
+    gender: 'Male',
+    'catch-phrase': 'ooga bunga',
+    icon_uri: 'https://acnhapi.com/v1/icons/villagers/1',
+    image_uri: 'https://acnhapi.com/v1/images/villagers/1',
+  },
+];
