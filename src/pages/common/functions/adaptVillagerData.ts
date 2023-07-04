06179073cdc7d9b372202976e1bc023e09418489
Julien <julien.izzillo@gmail.com>
2023-07-04 22:09:38 +0200

    Translating villager gender

--- src/pages/common/functions/adaptVillagerData.ts
+++ src/pages/common/functions/adaptVillagerData.ts
@@ -2,5 +2,6 @@ import {Villager} from '../../villagers/VillagerType';
 import {translateSpecies} from './translateSpecies';
 
 export const adaptVillagerData = (villager: Villager) => {
+  villager.gender = villager.gender === 'Male' ? 'Mâle' : 'Femelle';
   villager.species = translateSpecies(villager.species);
 };
