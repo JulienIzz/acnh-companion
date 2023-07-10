76e4de385898c30ebcd78c1fdb0870d9e7365416
Julien <julien.izzillo@gmail.com>
2023-07-10 12:09:25 +0200

    Adding Screen type to navigator types

--- src/navigation/Types.tsx
+++ src/navigation/Types.tsx
@@ -1,6 +1,7 @@
 import {BugReportParams} from '../pages/bugreport/BugReporting';
 import {EncyclopedieParams} from '../pages/encyclopedie/Encyclopedie';
 import {HomeParams} from '../pages/home/HomeScreen';
+import {SeaCreaturesParams} from '../pages/seacreatures/SeaCreatures';
 import {SearchParams} from '../pages/search/SearchPage';
 import {VillagersParams} from '../pages/villagers/Villagers';
 
@@ -10,6 +11,7 @@ export type RootStackParamList = {
   Search: SearchParams;
   BugReporting: BugReportParams;
   Villagers: VillagersParams;
+  SeaCreatures: SeaCreaturesParams;
 };
 
 export type Routes = keyof RootStackParamList;
--- src/pages/seacreatures/SeaCreatures.tsx
+++ src/pages/seacreatures/SeaCreatures.tsx
@@ -4,6 +4,8 @@ import {Header} from '../header/Header';
 const HEADER_TEXT = 'Créatures Marines';
 const HEADER_IMAGE = require('../header/img/header_background.jpg');
 
+export type SeaCreaturesParams = undefined;
+
 export const SeaCreatures = () => {
   return <Header headerText={HEADER_TEXT} headerImagePath={HEADER_IMAGE} />;
 };
