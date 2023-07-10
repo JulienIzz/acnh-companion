3a02c26917dbd095b09e481fc5e014eb0ac19d58
Julien <julien.izzillo@gmail.com>
2023-07-10 17:24:19 +0200

    Conditionning screen rendering to data fetching

--- src/pages/seacreatures/SeaCreatures.tsx
+++ src/pages/seacreatures/SeaCreatures.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Header} from '../header/Header';
+import {useFetchSeaCreatures} from '../common/functions/fetchSeaCreatures';
+import {LoadingPage} from '../common/components/LoadingPage';
 
 const HEADER_TEXT = 'Créatures Marines';
 const HEADER_IMAGE = require('../header/img/header_background.jpg');
@@ -7,5 +9,10 @@ const HEADER_IMAGE = require('../header/img/header_background.jpg');
 export type SeaCreaturesParams = undefined;
 
 export const SeaCreatures = () => {
+  const seaCreatureList = useFetchSeaCreatures();
+  if (seaCreatureList.isLoading) {
+    return <LoadingPage image={HEADER_IMAGE} text={HEADER_TEXT} />;
+  }
+
   return <Header headerText={HEADER_TEXT} headerImagePath={HEADER_IMAGE} />;
 };
